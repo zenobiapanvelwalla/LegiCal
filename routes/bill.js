ce0af6cd7b0c47a1b67ecbde097273c9f44b5f1a
Sricheta Ruj <ruj@adobe.com>
2019-11-17 22:04:33 -0800

    add comments and watchlist API

--- routes/bill.js
+++ routes/bill.js
@@ -1,90 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-let Bill = require('../schemas/BillSchema');
-//https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html
-const axios = require('axios');
+var cassandra = require('cassandra-driver');
+var contactPoints = ['127.0.0.1'];
+var client = new cassandra.Client({contactPoints: contactPoints, keyspace:'cmpe295b', localDataCenter : 'datacenter1'});;
 
 var response = {
     statusCode:200,
     message:""
 };
-router.get('/',function(req,res,next){
-  
-  axios .get("https://api.legiscan.com/?key=B36e51861aaf4e8d544f86c1ce66fe98&op=getMasterList&state=CA")
-    .then(response => {
-      return res.send(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
-
-router.get('/search-bill/:state/:billNumber',function(req,res,next){
-  axios .get("https://api.legiscan.com/?key=B36e51861aaf4e8d544f86c1ce66fe98&op=search&state="+req.params.state+"&bill="+req.params.billNumber)
-    .then(response => {
-      return res.send(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
-
-router.get('/get-roll-call/:rollCallId',function(req,res,next){
-  axios .get("https://api.legiscan.com/?key=B36e51861aaf4e8d544f86c1ce66fe98&op=getRollcall&id="+req.params.rollCallId)
-    .then(response => {
-      return res.send(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
-//get bill text
-router.get('/get-bill-text/:docId',function(req,res,next){
-  axios .get("https://api.legiscan.com/?key=B36e51861aaf4e8d544f86c1ce66fe98&op=getBillText&id="+req.params.docId)
-    .then(response => {
-      return res.send(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
-
-//get all details of a particular bill
-//https://api.legiscan.com/?key=APIKEY&op=getBill&id=BILL_ID
-router.get('/:billId',function(req,res,next){
-  axios .get("https://api.legiscan.com/?key=B36e51861aaf4e8d544f86c1ce66fe98&op=getBill&id="+req.params.billId)
-    .then(response => {
-      return res.send(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
 
 
-router.post('/create', function(req, res, next) {
-      console.log("Inserting Bill");
-    
-      var billData = {
-        id: req.body.id,
-        title: req.body.title,
-        description: req.body.description,
-        dateIntroduced:req.body.dateIntroduced
-      }
-
-      Bill.create(billData, function (err, user) {
-        if (err) {
-            response.message = err;
+router.get('/getComments', function(req, res, next) {
+    let billId = req.body.billId;
+    var query = 'SELECT * FROM discussion where billId = ?';
+    execute(query, [billId], (err, result) => {
+        if(err) {
+            console.log("fail");
             response.statusCode = 500;
-            return res.send(response);
-          
-        } else {
-            response.message = "Bill Inserted"; 
-            return res.send(response);
-            
+            response.message = "Some internal error happened!"
+            res.send(response);
+        }else {
+            console.log("successfull");
+            response.statusCode = 200;
+            response.message = "Retrived comment succfully";
+            response.comments = result.rows[0];
+            res.send(response);
         }
-      });    
+    });
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -47,12 +47,13 @@ router.post('/login', function(req, res, next) {
                 res.send(response);
             }else if(password !== result.rows[0].password){
                response.statusCode = 500;
-               response.message = "Thats an icorrect password! Please try again."; 
+               response.message = "Thats an incorrect password! Please try again."; 
                res.send(response);
             }else{
                response.user = result.rows[0];
                var subquery = 'SELECT * FROM organization WHERE org_name = ?';
-               execute(subquery, [result.rows[0].orgName], (err, result) => {
+               console.log("****** result", result.rows[0]);
+               execute(subquery, [result.rows[0].orgname], (err, result) => {
                     if(err) {
                         console.log("fail");
                         response.statusCode = 500;
@@ -70,6 +71,49 @@ router.post('/login', function(req, res, next) {
         }
     }); 
 });
+
+router.post('/addComment', function(req, res, next) {
+
+    let billId = req.body.billId;
+    let comments = req.body.comments;
+
+    var query = 'INSERT INTO discussion (billId,  comments, orgName) VALUES (?, ?);';
+    execute(query, [billId, comments], (err, result) => {
+        if(err) {
+            console.log("fail");
+            response.statusCode = 500;
+            response.message = "Some internal error happened!"
+            res.send(response);
+        }else {
+            console.log("successfull");
+            response.statusCode = 200;
+            response.message = "successfully posted comments"
+            res.send(response);
+        }
+    });
+});
+
+router.post('/watchlist/create', function(req, res, next) {
+    let bill = req.body.bill;
+    let email = req.body.email;
+    let arr = [];
+    arr.push(bill);
+    var query = 'UPDATE USER SET savedbills = savedbills +  ? WHERE email = ?';
+    execute(query, [arr, email], (err, result) => {
+        if(err) {
+            console.log("fail");
+            response.statusCode = 500;
+            response.message = "Some internal error happened!";
+            res.send(response);
+        }else {
+            console.log("successfull");
+            response.statusCode = 200;
+            response.message = "successfully added to watlist";
+            res.send(response);
+        }
+    });
+});
+
 
 //Ensure all queries are executed before exit
 function execute(query, params, callback) {
